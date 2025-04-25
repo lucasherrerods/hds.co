72f9c96ac287cdceee2525b2b50eb9448b9f6005
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-04-24 23:49:04 -0300

    feat: apply discount for products priced over 70

--- src/components/NewArrivals/index.jsx
+++ src/components/NewArrivals/index.jsx
@@ -28,18 +28,25 @@ export default function NewArrivals() {
       <h2 className="text-3xl uppercase tracking-widest font-bold text-center py-2">
         Novidades
       </h2>
-      <ul className="flex gap-6 items-center justify-around py-20 flex-nowrap min-w-max">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 py-12 px-4">
         {products && products.map((item) => (
-          <li key={item.title} className="p-4 bg-[#F6F6F6] w-54 h-auto rounded-lg flex flex-col items-center justify-center gap-4 cursor-pointer transition-all duration-300 shadow-lg hover:scale-105 hover:shadow-lg">
-            <img src={item.thumbnail} alt="Image Product" />
+          <li key={item.title} className="p-4 bg-[#F6F6F6] rounded-xl flex flex-col items-center justify-center gap-4 cursor-pointer transition-all duration-300 shadow-lg hover:scale-105 hover:shadow-lg">
+            <img src={item.thumbnail} alt="Image Product" className="h-44 object-container" />
             <p className="text-sm tracking-wider">{item.title}</p>
             <div className="flex items-center w-full gap-2">
               <Star size={14} className="text-yellow-500 font-bold" />
               <p className="text-sm">{item.rating}<span className="font-light text-gray-600">/5</span></p>
             </div>
-            <div className="flex items-center gap-8 w-full">
-              <p className="font-bold">R$ {item.price}</p>
-              <p className="">R$ {item.price}</p>
+            <div className={`flex items-center gap-8 w-full ${item.price > 70 ? 'justify-end flex-row-reverse' : ''}`}>
+              <p className={`${item.price > 70 ? 'text-xs bg-[#ff333339] text-red-400 font-bold p-1 rounded-full' : 'hidden'}`}>
+                {item.price > 70 ? `-${item.discountPercentage}%` : ''}
+              </p>
+              <p className={`${item.price > 70 ? 'font-light line-through' : 'font-bold'}`}>
+                R$ {item.price}
+              </p>
+              <p className="font-bold">
+                {item.price > 70 ? `R$ ${Math.round(item.price - (item.price * (item.discountPercentage / 100))).toFixed(2)}` : ''}
+              </p>
             </div>
           </li>
         ))}
