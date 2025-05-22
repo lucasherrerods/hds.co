0bf6f0f0ffb2a199f532c772ab3b536768139497
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-05-21 22:46:13 -0300

    fix: correct the initial value from Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,23 +1,21 @@
 import { useState, useEffect } from "react"
 
 export default function Counter({ value }) {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(value ? 1 : 0) //Se tem estoque começa no 1, se não 0
 
   useEffect(() => {
-    if (!value) {
-      setCount(0)
-    }
+    setCount(value ? 1 : 0)
   }, [value])
 
   return (
     <div className="flex items-center gap-6 bg-[#F6F6F6] shadow-inner px-4 py-2 rounded-full">
-      <button disabled={!value} onClick={() => setCount((prev) => Math.max(1, prev - 1))} className={`font-bold text-xl text-gray-700 transition ${!value ? 'disabled' : 'cursor-pointer hover:text-black'}`}>
+      <button disabled={!value} onClick={() => setCount((prev) => Math.max(1, prev - 1))} className={`font-bold text-xl text-gray-700 transition ${!value || count <= 1 ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer hover:text-black'}`}>
         -
       </button>
-      <p className={`text-lg ${!value ? 'font-light' : 'font-medium'}`}>
+      <p className={`text-lg ${!value ? 'opacity-50' : 'font-medium'}`}>
         {count}
       </p>
-      <button disabled={!value} onClick={() => setCount((prev) => prev += 1)} className={`font-bold text-xl text-gray-700 transition ${!value ? 'disabled' : 'cursor-pointer hover:text-black'}`}>
+      <button disabled={!value} onClick={() => setCount((prev) => prev += 1)} className={`font-bold text-xl text-gray-700 transition ${!value ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer hover:text-black'}`}>
         +
       </button>
     </div>
